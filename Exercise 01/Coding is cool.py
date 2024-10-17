3368dca158fb977d54b5fe20e35daa933f5ce525
Taha-31 <tahabilal20181786@gmail.com>
2024-10-17 10:13:29 +0400

    Coding is cool
    exercise 01 of the first assessment.

---
+++ Exercise 01/Coding is cool.py
@@ -0,0 +1,19 @@
+# Exercise 1: Coding is Cool- 10 Marks
+# Fill in the blanks in the Python code below to output the phrase “Coding is
+# Cool” to the console using variables and string concatenation.
+# Fill in the blanks below
+# word1 =
+# word2 =
+# word3 =
+# # Use string concatenation to combine the variables and print the phrase
+# _____(_________)
+
+
+
+word1 = "coding" # first vairable 
+word2 = "is" # second vairable           ## strings saved as variables, word 1, word 2 and word 3
+word3 = "cool" # third variable
+
+print(word1, word2, word3) # print statement that concatenates the string variables using commas (,) and prints the statement
+print(word1 + " " + word2 + " "  + word3) # print statement that concatenates the string variables using addition operator (+),
+#                                           and prints the statement
