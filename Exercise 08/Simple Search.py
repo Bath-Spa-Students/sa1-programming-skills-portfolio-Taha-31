a3c403733f2354716e6f36ef687c6a05037e9e01
Taha-31 <tahabilal20181786@gmail.com>
2024-11-02 21:12:16 +0400

    Simple Search

---
+++ Exercise 08/Simple Search.py
@@ -0,0 +1,16 @@
+# Exercise 8: Simple Search - 30 Marks
+# Write a program that searches for a specific string within a list of strings. The list
+# is initialized with specific names (“Jake” “Zac”, “Ian”, “Ron”, “Sam”, “Dave”).
+# , and your task is to search for “Sam”.
+# Optional Requirements:
+# 1. Allow the user to input the search term instead of using a predefined value.
+# 2. Implement the search functionality based on user input.
+
+names = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"] # list of the names as mentioned in the question
+
+name_search = input("Enter the name you want to search for: ") # asks the user to input the name they're searching for
+
+if name_search in names: 
+    print(f"{name_search} was found in the list.") # if name is found, displays on the screen that the name youre looking for is in the list
+else:
+    print(f"{name_search} was not found in the list.") # if name isnt, displays on the screen that the name youre looking for is NOT in the list
