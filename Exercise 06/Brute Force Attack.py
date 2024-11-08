7d65e498cabe9bec4e915c2b11d8145eb6c97bdc
Taha-31 <tahabilal20181786@gmail.com>
2024-11-08 04:59:14 +0400

    Brute Force Attack

--- Exercise 06/Brute Force Attack.py
+++ Exercise 06/Brute Force Attack.py
@@ -16,35 +16,36 @@
 # maximum number of attempts is reached, inform the user that the authorities
 # have been alerted.
 
-correct_password = "12345" # correct password is defined as a string since there is no comparison operators in the program
+right_password = "12345" # correct password is defined as a string since there is no comparison operators in the program
+username = input("Enter your username: ")
 
+counter = 0 # counter for tracking the number of attempts
 
-max_attempts = 5 # maximum number of allowed attempts
-attempts = 0 # counter for tracking the number of attempts
+print("-------------------------------------------------------")
+print("------------------- PASSWORD ENTRY --------------------")
+print("-------------------------------------------------------")
+print("-------You have 5 attempts to unlock this phone.-------")
+print("-------------------------------------------------------")
 
-print("-----------------------")
-print("-- PASSWORD ENTRY --")
-print("-----------------------")
-
-while attempts < max_attempts: # start a while loop to repeatedly ask for the password
+while True: # start a while loop to repeatedly ask for the password
     print("Enter a 5 digit password to gain access.") # displays to tell us what the user should do
     password = input("Enter the password: ") # User input for the correct password
     
-    if password == correct_password: # check if the entered password is correct 
-        print("Access granted, Welcome!")
-        print("-----------------------")
-        print("-- ACCESS GRANTED --")
-        print("-----------------------")
+    if password == right_password: # check if the entered password is correct 
+        print(f"Access granted, Welcome {username}!")
+        print("-------------------------------------------------------")
+        print("-------------------- ACCESS GRANTED -------------------")
+        print("-------------------------------------------------------")
         break # exit the loop if the correct password is entered
     else:
-        attempts += 1 # adds 1 to the counter
-
-        remaining_attempts = max_attempts - attempts # calculates and informs the user of how many attempts remaining
+        counter += 1 # adds 1 to the attempts/counter
+        remaining_attempts = 5 - counter # calculates and informs the user of how many attempts remaining
         if remaining_attempts > 0:
             print(f"Incorrect password, You have {remaining_attempts} attempts remaining.")
         else:
             print(f"Maximum amount of attempts reached, The authorities have been alerted")
-            print("-----------------------")
-            print("-- ACCESS DENIED --")
-            print("-----------------------")
+            print("-------------------------------------------------------")
+            print("-------------------- ACCESS DENIED --------------------")
+            print("-------------------------------------------------------")
+            break # exit loop since all attempts are used
         
