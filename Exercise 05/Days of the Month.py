a29feb5034c987471bfef1df1a8a024c58a09706
Taha-31 <tahabilal20181786@gmail.com>
2024-11-08 04:59:05 +0400

    Days of the Month

--- Exercise 05/Days of the Month.py
+++ Exercise 05/Days of the Month.py
@@ -15,14 +15,32 @@
 # accordingly.
 
 days_in_month = {
-    1: 31, 2: 29, 3: 31, 4: 30, 5: 31, 6: 30,
-    7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31} # dictionary mapping month numbers to number of days (assuming February has 28 days by default)
+    1: 31,
+    2: 29,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30, 
+    10: 31,
+    11: 30,
+    12: 31} # dictionary mapping month numbers to number of days (assuming February has 28 days by default)
 
 month_names = {
-    1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June",
-    7: "July", 8: "August", 9: "September", 10: "October", 11: "November", 12: "December"} # dictionary mapping month numbers to month names
-
-
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December"} # dictionary mapping month numbers to month names
 
 is_running = True
 
