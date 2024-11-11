d2119f4e3163e19a92228842fabcf8c808df65fd
Taha-31 <tahabilal20181786@gmail.com>
2024-11-11 18:58:35 +0400

    Hello
    Exercise 9 - Hello

---
+++ Exercise 09/Hello.py
@@ -0,0 +1,20 @@
+# Exercise 9: Hello - 10 Marks
+# Fill in the blanks in the code below so that the function hello() prints “Hello”
+# to the console.
+
+# def hello():
+# ____ # Fill in this blank to print "Hello" to the console
+# def main():
+# ____ # Fill in this blank to call the hello() function
+# if __name__ == "__main__":
+# main()
+
+def hello():                    # defines the hello function
+    print("Hello")              # prints "hello" under the hello() function
+
+def main():                     # defines main function
+    hello()                     # runs the hello() function
+
+if __name__ == "__main__":
+    main()                      # runs the main function
+
