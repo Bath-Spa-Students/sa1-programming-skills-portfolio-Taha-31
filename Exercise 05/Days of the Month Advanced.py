b75cce84addfe771474958c16e8f1b81fd4a165f
Taha-31 <tahabilal20181786@gmail.com>
2024-11-11 19:39:53 +0400

    Days of the Month Advanced
    Exercise 05 - Days of the Month ---Advanced Requirement

--- Exercise 05/Days of the Month Advanced.py
+++ Exercise 05/Days of the Month Advanced.py
@@ -36,19 +36,24 @@ month_names = {
 
 is_running = True
 
-while is_running: # Start a loop to prompt for month number input until valid input is given
+while is_running: # start a loop to prompt for month number input until valid input is given
     month = int(input("Enter the month number you want the days of (1-12): ")) # asks the user to input the month number
-    if 1 <= month <= 12: # check if the input is valid
+    if 1 <= month <= 12: # check if the input is in between 1 and 12
         if month == 2: # check if February needs a leap year adjustment
-            is_leap_year = input("Is it a leap year? (yes/no): ").lower()
-            if is_leap_year == "yes" and is_leap_year == "no":
-                days = 29 if is_leap_year == "yes" else 28
+            leap_year = input("Is it a leap year? (yes/no): ").lower()
+            if leap_year == "yes":
+                days = 29
+                print(f"The number of days in {month_names[month]} is {days}")
+                is_running = False # exit loop since everything is valid
+            elif leap_year == "no":
+                days = 28
                 print(f"The number of days in {month_names[month]} is {days}")
                 is_running = False # exit loop since everything is valid
             else:
-                print("Thats not a valid answer.")
+                print("Enter a valid answer.")
         else:  # print the number of days for months other than February
             print(f"The number of days in {month_names[month]} is {days_in_month[month]}.")
             is_running = False # exit loop since everything is valid
     else: # else statement if the number inputted is outside of range
         print("Invalid month number, Please enter a number between 1 and 12")
+
