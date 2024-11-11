8e1bcf3b1d5246b9939670444e93eb5c5c2d4a83
Taha-31 <tahabilal20181786@gmail.com>
2024-11-11 20:01:09 +0400

    Primitive Quiz Advanced
    Exercise 04 - Primitive Quiz
    Advanced Requirement

--- Exercise 04/Primitive Quiz Advanced.py
+++ Exercise 04/Primitive Quiz Advanced.py
@@ -28,15 +28,14 @@ count = 0  # tracks the current question number
 for question in questions:  # loops through and displays each question one by one
     print("----------------------------------")  # prints a line for visual separation
     print(question)  # prints the current question
-    guess = input(
-        "Enter your answer: ").lower()  # asks the user for an answer and converts it to lowercase to ignore capitalization
+    guess = input("Enter your answer: ").lower()  # asks the user for an answer and converts it to lowercase to ignore capitalization
     guesses.append(guess)  # adds the user's answer to the "guesses" list
     if guess == answers[count]:  # Checks if the user's answer matches the correct answer in "answers" tuple
         score += 1  # increases score by 1 if the answer is correct
         print("CORRECT!")  # informs the user their answer is correct
     else:  # executes if the answer is incorrect
         print("INCORRECT!")  # informs the user their answer is incorrect
-        print(f"{answers[count]} is the correct answer.")  # Displays the correct answer
+        print(f"{answers[count]} is the correct answer.")  # displays the correct answer
     count += 1  # +1 to the count, so the counter can move to the next question
 
 print("-------------------------------------------------------------------------------------")
@@ -46,13 +45,11 @@ print("-------------------------------------------------------------------------
 print("answers: ", end="")  # prepares to print all correct answers on the same line
 for answer in answers:  # loops through correct answers
     print(answer, end=" ")  # prints each answer in one line
-print()
-print("-------------------------------------------------------------------------------------")  # creates a new line
+print() # creates a new line
+print("-------------------------------------------------------------------------------------")  
 print("guesses: ", end="")  # prepares to print all user guesses on the same line
 for guess in guesses:  # loops through user guesses
     print(guess, end=" ")  # prints each guess in one line
-print()
-print("-------------------------------------------------------------------------------------")  # creates a new line
-
-score = int(score / len(questions) * 100)  # Calculates score as a percentage
-print(f"Your score is: {score}%")  # displays the user's score as a percentage
+print() # creates a new line
+print("-------------------------------------------------------------------------------------")  
+print(f"Your score is: {score}/10")  # displays our score
