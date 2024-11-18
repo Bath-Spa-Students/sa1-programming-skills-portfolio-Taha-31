a73408bdb22269a592bd7d1c5bf7a8b4287c4cac
Taha-31 <tahabilal20181786@gmail.com>
2024-11-18 11:36:34 +0400

    Update calculations.py

---
+++ Code lab 1 practice/calculations/calculations.py
@@ -0,0 +1,26 @@
+# performing calculations
+
+a = 90
+b = 75
+c = a + b
+print(c) # addition
+
+d = 80
+e = 79.5
+f = d - e
+print(f) # subtraction
+
+g = 20
+h = 5
+i = g * h
+print(i) # multiplication 
+
+j = 50
+k = 2
+l = j/k
+print(l) # division but the answer comes out as a float since "/" is used
+
+m = 125
+n = 9
+o = m//n
+print(o) # division but the answer comes out as a int since "//" is used
