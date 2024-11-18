bcc12d603654c8d902c0d7afd7fc4f9aaa1e8544
Taha-31 <tahabilal20181786@gmail.com>
2024-11-18 11:39:30 +0400

    Brute Force Attack
    Exercise 06 - Brute Force Attack

--- Exercise 06/Brute Force Attack.py
+++ Exercise 06/Brute Force Attack.py
@@ -2,8 +2,6 @@
 # Write a program that simulates a password entry system. The correct password
 # is defined as 12345. The program should keep asking the user to enter the
 
-# 3
-
 # password until they provide the correct one.
 # Basic Requirements:
 # 1. Define the correct password.
