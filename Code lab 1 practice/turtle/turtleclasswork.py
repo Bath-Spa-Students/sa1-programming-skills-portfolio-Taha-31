8fc1fed702c6211a4382afbfd030398c594f91a3
Taha-31 <tahabilal20181786@gmail.com>
2024-11-18 11:38:17 +0400

    Turtleclasswork

---
+++ Code lab 1 practice/turtle/turtleclasswork.py
@@ -0,0 +1,18 @@
+import turtle
+
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.right(50)
+turtle.forward(100)
+turtle.left(100)
+turtle.forward(400)
+
+turtle.pensize(5)
+turtle.circle(100)
+
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(100)
