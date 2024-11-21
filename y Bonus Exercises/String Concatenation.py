39fcc39bba98a36246393b6151f4e2e334f128b9
Taha-31 <tahabilal20181786@gmail.com>
2024-11-22 01:44:28 +0400

    String concatenation

---
+++ y Bonus Exercises/String Concatenation.py
@@ -0,0 +1,10 @@
+# Strings Concatenation:
+# Write three strings in different variables and print the output as one string.
+
+student_id = 124129
+student_name = "Taha"
+student_age = 17
+
+print("Student id:", student_id,
+      "Student Name:", student_name,
+       "Student Age:", student_age )
