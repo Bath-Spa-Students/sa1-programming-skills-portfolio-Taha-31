278224723936a953b486ad9f6b69e9ade9e995b8
Taha-31 <tahabilal20181786@gmail.com>
2024-11-22 01:44:16 +0400

    List - Names

---
+++ y Bonus Exercises/List - Names.py
@@ -0,0 +1,11 @@
+# List -Names
+# Store the names of a few of your friends in a list called names. Print each person’s name
+# by accessing each element in the list, one at a time.
+
+friend_names = ["Amaan", "Miguel", "Vicfirth", "Kurt", "Dawna"]
+
+print(f"Friend number 1: {friend_names[0]}")
+print(f"Friend number 2: {friend_names[1]}")
+print(f"Friend number 3: {friend_names[2]}")
+print(f"Friend number 4: {friend_names[3]}")
+print(f"Friend number 5: {friend_names[4]}")
