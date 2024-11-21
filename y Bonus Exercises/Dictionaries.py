c53126288ebad0a6dd4d93c011c97157866be236
Taha-31 <tahabilal20181786@gmail.com>
2024-11-22 01:43:57 +0400

    Dictionaries

---
+++ y Bonus Exercises/Dictionaries.py
@@ -0,0 +1,12 @@
+# Dictionaries:
+# Use a dictionary to store information about a person you know.Store their first name,
+# last name, age, and the city in which they live.
+
+# You should have keys such as first_name, last_name, age, and city. Print each piece of
+# information stored in your dictionary.
+
+friend_info = {"First name": "Maleeha",
+               "Second name": "Asghar",
+               "City": "London"}
+
+print(f"First name: {friend_info['First name']} \nSecond name: {friend_info['Second name']} \nCity: {friend_info['City']}")
